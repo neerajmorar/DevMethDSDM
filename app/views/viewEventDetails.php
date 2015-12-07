3d8d20ba909079840a58d26643ed1fba84ded85b
neerajmorar <neerajmorar@hotmail.co.uk>
2015-12-07 16:55:06 +0000

    Added exception handling on viewEventDetails.php

--- app/views/viewEventDetails.php
+++ app/views/viewEventDetails.php
@@ -11,7 +11,16 @@ require '../public/_header.php';
 <div id="content">
     <h2><?php echo $pageTitle; ?></h2>
     
-    <?php echo $data[0]["name"] . " " . $data[0]["description"]; ?>
+    <?php 
+        if (isset($data[0]))
+        {
+            echo $data[0]["name"] . " " . $data[0]["description"]; 
+        }
+        else 
+        {
+            echo "This event doesn't exist.";
+        }
+    ?>
 </div>
    
 </body>
